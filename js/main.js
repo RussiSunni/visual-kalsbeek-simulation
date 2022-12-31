a3fc6af18c6367ed145aa4e74eb43441abab48da
RussiSunni <jonathandyason@hotmail.com>
2022-12-31 15:10:51 +0200

    sdf

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ window.onload = function () {
         width: 800,
         height: 600,
         parent: 'phaser-game',
+        backgroundColor: '#000000',
 
         scene: [MenuScene, ParametersScene, LeftTaskScene, RightTaskScene, BothTasksScene]
     };
